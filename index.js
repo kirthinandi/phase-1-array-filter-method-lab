d5ad24254192d2f7894aa9ed110d9aa4eba30d0a
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-24 16:49:26 -0700

    commit tree fa146b47033252b172f95336e04887e976cd0ea8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(driver, name) {
+    return driver.filter(function(dn) {
+        return dn.toLowerCase().indexOf(name.toLowerCase()) !== -1
+    })
+}
+
+function fuzzyMatch(driver, name) {
+    return driver.filter(function(dn) {
+        return dn.toLowerCase().slice(0, 2).indexOf(name.toLowerCase()) !== -1
+    })
+}
+
+function matchName(driver, string) {
+    /* for (const drivers of driver) {
+        if (drivers.name === string) {
+            return drivers;
+        }
+    }
+    */
+    return driver.filter(word => word.name === string);
+}
